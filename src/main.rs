8532b3ef37e18e8b280b57b535aa17464c593185
Tokusei Noborio <tokusei@nyampass.com>
2025-11-28 14:52:36 +0900

    refactor: update server binding to use TcpListener for improved flexibility

--- src/main.rs
+++ src/main.rs
@@ -55,9 +55,8 @@ async fn main() -> anyhow::Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     info!(?addr, "starting server");
 
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await?;
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    axum::serve(listener, app).await?;
 
     Ok(())
 }
